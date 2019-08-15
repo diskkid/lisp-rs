29fef75842d28a8725926af09c982e20fa5a16a7
Daisuke Koide <disk2id@gmail.com>
2019-08-15 14:45:21 +0900

    Remove unused function

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use std::collections::vec_deque::VecDeque;
-
 use lisp_rs::env::Env;
 use lisp_rs::token;
 use lisp_rs::value;
-use lisp_rs::value::{LispList, LispValue};
 use std::env;
 use std::io::stdin;
-
-fn call_function(func: String, args: &LispList, env: &Env) -> LispValue {
-    if let Some(f) = env.get(&func) {
-        match f {
-            LispValue::Lambda(f) => return f(args),
-            _ => {}
-        }
-    }
-    let mut list = args.clone();
-    list.push_front(LispValue::Symbol(func));
-    LispValue::List(list)
-}
 
 fn main() {
     let env = Env::global();
